dcb2f04888c6e008730773c06668fe63193a69c2
Adeesha <adeeshadilmira2@gmail.com>
2026-04-03 18:08:00 +0530

    fixed error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}/api` });
 
 // Automatically attach JWT token from localStorage to every request
 API.interceptors.request.use((config) => {
